2f8130cc6a07cff039f5f89e9222509ef9cf42a7
chriscauley <chris@lablackey.com>
2019-03-14 07:20:14 -0400

    briefly flirting with using bash to write html (its bad)

--- gifit.sh
+++ gifit.sh
@@ -3,7 +3,7 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
-DIR=".cache"
+ROOT=".cache"
 
 filename=$(basename -- "$1")
 extension="${filename##*.}"
@@ -55,35 +55,48 @@ done
 if [ "$clear" = "1" ]
    then
        echo "cleared cache directory"
-       rm -rf $DIR
-       mkdir $DIR
+       rm -rf $ROOT
+       mkdir $ROOT
 fi
 
-function _html () {
-    echo "<$1>$2</$1>" >> "$html_file"
+function _done () {
+    cat $html_file
+    python -m SimpleHTTPServer 8756
+    exit
+}
+
+function _new_dir () {
+    _h4 $1
+    $DIR=$ROOT/$filename$/$1
+    export SOURCE=$DIR/$filename.$extension
+}
+
+function _h4 () {
+    echo "</div><div><h4>$1</h4>" >> "$html_file"
 }
 
 function _img () {
     echo "<img title='$1' src='../$1'/>" >> "$html_file"
 }
 
-mkdir -p $DIR/$filename
-rm -f $DIR/$filename/*
+mkdir -p $ROOT/$filename
+rm -f $ROOT/$filename/*
 
-html_file="$DIR/$filename.html"
-rm -f $html_file
-touch $html_file
+html_file="$ROOT/$filename.html"
+echo "<div style='display:flex;justify-content: space-between'><div>" >  $html_file
 
-SOURCE="$DIR/$filename.$extension"
+SOURCE="$ROOT/$filename.$extension"
 cp $filename.$extension $SOURCE
-_html h4 $SOURCE
+_h4 $SOURCE
 _img $SOURCE
-open $html_file
-exit
+
 if [ ! -z "$brighten" ]
    then
-       convert $SOURCE -modulate $brighten% $SOURCE
+       SOURCE_2=`_new_dir brighten`
+       convert $SOURCE -modulate $brighten% $SOURCE_2
+       SOURCE=$SOURCE_2
        echo "brightened image"
+       _img $SOURCE
 fi
 
 if [ ! -z "$negate" ]
@@ -96,7 +109,9 @@ for i in `seq $nframes`
 do
     HUE=$((200*i/$hue_rate))
     N=`printf %03d $i`
-    convert $SOURCE -modulate 100,100,$HUE $DIR/$filename/$N.png
+    convert $SOURCE -modulate 100,100,$HUE $ROOT/$filename/$N.png
 done
 
-convert -delay $delay -loop 0 $DIR/$filename/*.png party-$filename.gif
+convert -delay $delay -loop 0 $ROOT/$filename/*.png party-$filename.gif
+_done
+
