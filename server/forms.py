b94fbf610ebac9665f8855d1169dde379672b314
chriscauley <chris@lablackey.com>
2019-04-04 12:03:33 +0000

    Validate party image server side using a form

---
+++ server/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from server.models import SourceImage
+
+class PartyImageForm(forms.Form):
+    resize = forms.IntegerField(required=False)
+    _nc = ['','red','green','blue']
+    NEGATE_CHOICES = zip(_nc,_nc)
+    negate = forms.ChoiceField(required=False,choices=NEGATE_CHOICES)
+    n_frames = forms.IntegerField(required=False)
+    source = forms.ModelChoiceField(queryset=SourceImage.objects.all())
