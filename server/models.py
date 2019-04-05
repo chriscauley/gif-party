1350d4f9d5e5519ce002239379523aa4eed0bb37
chriscauley <chris@lablackey.com>
2019-04-05 04:11:25 +0000

    caching colors and n_frames on SourceImage

--- server/models.py
+++ server/models.py
@@ -15,15 +15,17 @@ class AbstractModel(JsonModel):
             "{} #: {}".format(self.__class__.__name__, self.id)
         )
 
+def run(args):
+    process = Popen(args, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    if stderr:
+        raise Exception(stderr.decode("utf-8"))
+    return stdout.decode("utf-8")
 
 def partify(src_path, data):
     args = ['bash', 'gifit.sh', src_path, get_short_args(data)]
     args += get_args(data)
-    process = Popen(args, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout.decode("utf-8"))
-    if stderr:
-        print(stderr.decode("utf-8"))
+    run(args)
 
 
 def get_args(data):
@@ -47,12 +49,55 @@ def get_short_args(data):
 class SourceImage(models.Model):
     name = models.CharField(max_length=32)
     src = models.ImageField(upload_to="source_images")
+    colors = JSONField(default=list)
+    n_frames = models.IntegerField(default=0)
+
     @property
     def as_json(self):
+        keys = ['id','name','colors','is_gif']
         return {
             'id': self.id,
             'name': self.name,
             'src': self.src.url,
         }
+
     def __str__(self):
         return self.name
+
+    def save(self,*args,**kwargs):
+        self.n_frames = len(run(['identify',self.src.path]).strip().split('\n'))
+        #print(self.src.path.split('/')[-1],self.n_frames)
+
+        # get raw histogram
+        histogram = run(['convert',self.src.path,'-format','%c','histogram:info:'])
+
+        # trim whitespace, remove empty lines
+        histogram = [l.strip() for l in histogram.split('\n') if l]
+
+        # sort by most occurrence
+        histogram = sorted(
+            histogram,
+            key=lambda s: int(s.split(':')[0]),
+            reverse=True
+        )
+
+        # ignore zero alpha
+        histogram = [l for l in histogram if not ',  0)' in l]
+
+        self.colors = []
+        matched = []
+        for line in histogram:
+            if len(self.colors) > 5:
+                break
+            count = int(line.split(':')[0])
+            color = line.split(' ')[-1]
+            if color in matched:
+                continue
+            matched.append(color)
+            self.colors.append({
+                'color': color,
+                'count': count,
+            })
+            #print(count,color)
+
+        super().save(*args,**kwargs)
