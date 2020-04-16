d983079ca9887b854841fb96b546e4aa00208c62
chriscauley <chris@lablackey.com>
2020-04-16 22:54:20 +0000

    styling home page and adding nav

--- app/app.js
+++ app/app.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, HashRouter, Route, Link } from 'react-router-dom'
 
 import Home from './screens/Home'
+import Nav from './components/Nav'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <div className="container mx-auto">
-          <BrowserRouter>
-            <Route exact path="/" component={Home} />
-          </BrowserRouter>
-          <HashRouter>
-          </HashRouter>
-        </div>
+      <div className="container mx-auto">
+        <Nav />
+        <BrowserRouter>
+          <Route exact path="/" component={Home} />
+        </BrowserRouter>
+        <HashRouter>
+        </HashRouter>
       </div>
     )
   }
---
+++ app/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default () => {
+  return (
+    <header className="navbar p-4 flex justify-between items-center bg-gray-100">
+      <section className="navbar-section">
+        <a href="/" className="navbar-brand mr-2 font-bold text-blue-700 text-3xl">GTFO.party</a>
+      </section>
+      <section className="navbar-section">
+        <a className="btn btn-primary" href="https://github.com/chriscauley/gif-party/">github</a>
+      </section>
+    </header>
+  )
+}
