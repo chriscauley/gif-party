80095dea86ab4dbac815165d34fc57ba182b3d78
chriscauley <chris@lablackey.com>
2020-04-20 20:08:49 +0000

    adding admin for PartyImage

--- server/admin.py
+++ server/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from server.models import SourceImage
+from server.models import SourceImage, PartyImage
 
 @admin.register(SourceImage)
 class SourceImageAdmin(admin.ModelAdmin):
     readonly_fields = ['colors','n_frames']
+
+@admin.register(PartyImage)
+class PartyImageAdmin(admin.ModelAdmin):
+    pass
