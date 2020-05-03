a5b3f1af9c058d7f897689148a67364a5ae75d78
chriscauley <chris@lablackey.com>
2020-05-03 15:41:07 +0000

    switching to ur for scripts

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
-if __name__ == '__main__':
+
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -10,4 +12,12 @@ if __name__ == '__main__':
         raise ImportError("Couldn't import Django. Are you sure it's installed and "
                           "available on your PYTHONPATH environment variable? Did you "
                           "forget to activate a virtual environment?") from exc
+
+    runserver.default_port = "8379"
+    runserver.default_addr = "0.0.0.0"
+
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
